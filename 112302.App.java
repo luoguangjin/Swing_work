8ae3cf03111feba5a81a1c2758f4eb2c85e84bcf
luoguangjin <67535596+luoguangjin@users.noreply.github.com>
2020-11-23 12:02:21 +0800

    1秒自动生成随机坐标

---
+++ 112302.App.java
@@ -0,0 +1,13 @@
+package com.jackluo;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+
+        int x,y;
+        while (true){    Thread.sleep(1000);
+            x=(int)(Math.random()*100);
+            y=(int)(Math.random()*100);
+            System.out.println("坐标："+(x+","+y));
+        }
+    }
+}

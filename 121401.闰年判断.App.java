3c763cf1f7b1c63008dbab84c82d7fa06e527b37
luoguangjin <67535596+luoguangjin@users.noreply.github.com>
2020-12-14 11:43:16 +0800

    Update 121401.闰年判断.App.java

--- 121401.闰年判断.App.java
+++ 121401.闰年判断.App.java
@@ -7,11 +7,14 @@ public class Test {
             System.out.println("请输入一个年份：");
             Scanner scanner = new Scanner(System.in);
             year = scanner.nextInt();
-            if(year<0 || year >3000){
+            if(year<-3200 || year >3200){
                 System.out.println("请输入正确的年份！");
             }
-            if(year%4==0&&year%100!=0||year%400==0){
-                System.out.println(year+"年是闰年");
+            else if(year>0 && year%4==0&&year%100!=0||year%400==0 ){
+                System.out.println("公元后"+year+"年是闰年");
+            }
+            else if(year%4==-1 || year%400==1){
+                System.out.println("公元前"+year+"年是闰年");
             }else{
                 System.out.println(year+"年不是闰年");
             }
